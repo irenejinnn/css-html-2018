7168046cf39e2c30e2a9940c176392e227854647
svanyc <svanyc@uca-206-05.computerart.net>
2018-12-10 21:07:24 -0500

    add sass example

---
+++ week11/sass-example/src/components/header/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.scss';
+
+const Header = () => {
+    return (
+        <header className="Header">
+        <h2 className="Header--title">Header with Sass</h2>
+        </header>
+    );
+};
+
+export default Header;
